2eeb020c0f77cd2c4ce2a898ed412885f0729213
Matthew <matthew.holmes111@gmail.com>
2025-02-27 17:32:49 +0000

    resinstate some necessary functions (TODO - get rid of these later)

--- LLM/llama/chat_format.py
+++ LLM/llama/chat_format.py
@@ -5,22 +5,22 @@
 # top-level folder for each specific model found within the models/ directory at
 # the top-level of this source tree.
 
-# https://github.com/meta-llama/llama-models/blob/main/models/llama3/api/chat_format.py
-
+import io
 import uuid
 
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Tuple
 
-
 from .datatypes import (
     BuiltinTool,
     RawContent,
+    RawMediaItem,
     RawMessage,
     RawTextItem,
     Role,
     StopReason,
     ToolCall,
+    ToolPromptFormat,
 )
 
 from .tokenizer import Tokenizer
@@ -28,12 +28,17 @@ from .tokenizer import Tokenizer
 from .tool_utils import ToolUtils
 
 
+@dataclass
+class VisionInput:
+    mask: List[List[int]]
+    images: List
 
 
 @dataclass
 class LLMInput:
     tokens: List[int]
- 
+    vision: Optional[VisionInput] = None
+
 
 def role_str(role: Role) -> str:
     role_strs = {
@@ -62,7 +67,102 @@ class ChatFormat:
         tokens.extend(self.tokenizer.encode("\n\n", bos=False, eos=False))
         return tokens
 
- 
+    def encode_content(self, content: RawContent) -> LLMInput:
+        tokens, images = self._encode_content(content, bos=True)
+        return self._model_input_from_tokens_images(tokens, images)
+
+    def _encode_content(self, content: RawContent, bos: bool = False) -> Tuple[List[int], List]:
+        tokens = []
+        images = []
+
+        added_bos = False
+
+        def _process(c):
+            nonlocal added_bos, bos
+
+            if isinstance(c, str) or isinstance(c, RawTextItem):
+                if isinstance(c, RawTextItem):
+                    c = c.text
+                tokens.extend(self.tokenizer.encode(c, bos=False if added_bos else bos, eos=False))
+                added_bos = True
+
+            # elif isinstance(c, RawMediaItem):
+            #     bos = False if added_bos else bos
+            #     if bos:
+            #         tokens.append(self.tokenizer.special_tokens["<|begin_of_text|>"])
+            #         added_bos = True
+            #     tokens.append(self.vision_token)
+
+            #     bytes_io = io.BytesIO(c.data) if isinstance(c.data, bytes) else c.data
+            #     image = PIL_Image.open(bytes_io)
+            #     image = image.convert("RGB")
+            #     images.append(image)
+
+        if isinstance(content, list):
+            for c in content:
+                _process(c)
+        else:
+            _process(content)
+
+        return tokens, images
+
+    def encode_message(
+        self, message: RawMessage, tool_prompt_format: ToolPromptFormat
+    ) -> Tuple[List[int], List]:
+        tokens = self._encode_header(message.role)
+        images = []
+
+        def _process_content(c):
+            toks, imgs = self._encode_content(c)
+            tokens.extend(toks)
+            images.extend(imgs)
+
+        if (
+            message.role == "assistant"
+            and len(message.tool_calls) > 0
+            and message.tool_calls[0].tool_name == BuiltinTool.code_interpreter
+        ):
+            tokens.append(self.tokenizer.special_tokens["<|python_tag|>"])
+
+        _process_content(message.content)
+
+        if message.role == "user" and message.context is not None:
+            # This is RAG context; why is it here in the chat format? I don't think
+            # this is needed and can be moved upwards
+            _process_content("\n\n")
+            _process_content(message.context)
+
+        if message.role == "assistant":
+            for t in message.tool_calls:
+                content = ToolUtils.encode_tool_call(t, tool_prompt_format)
+                _process_content(content)
+
+        eom = False
+        if message.role == "assistant":
+            eom = message.stop_reason == StopReason.end_of_message
+
+        tokens.append(self.tokenizer.special_tokens["<|eom_id|>" if eom else "<|eot_id|>"])
+        return tokens, images
+
+    def encode_dialog_prompt(
+        self,
+        messages: List[RawMessage],
+        tool_prompt_format: Optional[ToolPromptFormat] = None,
+    ) -> LLMInput:
+        tool_prompt_format = tool_prompt_format or ToolPromptFormat.json
+        tokens = []
+        images = []
+        tokens.append(self.tokenizer.special_tokens["<|begin_of_text|>"])
+        for message in messages:
+            toks, imgs = self.encode_message(message, tool_prompt_format)
+            tokens.extend(toks)
+            images.extend(imgs)
+
+        # Add the start of an assistant message for the model to complete.
+        tokens.extend(self._encode_header("assistant"))
+
+        return self._model_input_from_tokens_images(tokens, images)
+
     # TODO(this should be generic, not only for assistant messages)
     def decode_assistant_message(self, tokens: List[int], stop_reason: StopReason) -> RawMessage:
         content = self.tokenizer.decode(tokens)
@@ -133,3 +233,42 @@ class ChatFormat:
             stop_reason=stop_reason,
             tool_calls=tool_calls,
         )
+
+    def _model_input_from_tokens_images(self, tokens: List[int], images: List) -> LLMInput:
+        vision_input = None
+        if len(images) > 0:
+            vision_input = VisionInput(
+                mask=create_vision_mask(tokens, self.vision_token),
+                images=images,
+            )
+
+        return LLMInput(
+            tokens=[128256 if token == self.vision_token else token for token in tokens],
+            vision=vision_input,
+        )
+
+
+def create_vision_mask(
+    tokens: List[int],
+    vision_token: int,
+) -> List[List[int]]:
+    vision_token_locations = [i for i, token in enumerate(tokens) if token == vision_token]
+    if len(vision_token_locations) == 0:
+        return []
+
+    if len(vision_token_locations) == 1:
+        # only one image present, unmask until end of sequence
+        return [[vision_token_locations[0], -1]]
+    vision_masks = [[loc1, loc2] for loc1, loc2 in zip(vision_token_locations[:-1], vision_token_locations[1:])]
+    # last image will attend to all subsequent text
+    vision_masks.append([vision_token_locations[-1], len(tokens)])
+
+    # if there are two or more consecutive vision tokens,
+    # they should all attend to all subsequent
+    # text present
+    last_mask_end = vision_masks[-1][1]
+    for vision_mask in vision_masks[::-1]:
+        if vision_mask[0] == vision_mask[1] - 1:
+            vision_mask[1] = last_mask_end
+        last_mask_end = vision_mask[1]
+    return vision_masks
